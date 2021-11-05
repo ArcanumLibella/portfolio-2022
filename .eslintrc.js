ea1f5bf480253473b56b274bcda275724ff9ff0f
ArcanumLibella <fiona.roux@hetic.net>
2021-11-05 17:25:59 +0100

    :tada: Init project with typescript and linter

---
+++ .eslintrc.js
@@ -0,0 +1,76 @@
+module.exports = {
+    root: true, // Make sure eslint picks up the config at the root of the directory
+    parserOptions: {
+      ecmaVersion: 2020, // Use the latest ecmascript standard
+      sourceType: 'module', // Allows using import/export statements
+      ecmaFeatures: {
+        jsx: true, // Enable JSX since we're using React
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect the react version
+      },
+    },
+    env: {
+      browser: true, // Enables browser globals like window and document
+      amd: true, // Enables require() and define() as global variables as per the amd spec.
+      node: true, // Enables Node.js global variables and Node.js scoping.
+    },
+    plugins: ['simple-import-sort', 'unused-imports', 'import'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:jsx-a11y/recommended',
+      'plugin:prettier/recommended', // Make sure this is always the last element in the array.
+    ],
+    rules: {
+      'no-console': ['warn', { allow: ['error'] }],
+      'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Use our .prettierrc file as source
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'jsx-a11y/click-events-have-key-events': 'warn',
+      'jsx-a11y/no-noninteractive-element-interactions': 'warn',
+      'jsx-a11y/no-static-element-interactions': 'warn',
+      'jsx-a11y/anchor-is-valid': [
+        'error',
+        {
+          components: ['Link'],
+          specialLink: ['hrefLeft', 'hrefRight'],
+          aspects: ['invalidHref', 'preferButton'],
+        },
+      ],
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^react'],
+            ['^\\u0000', '^@?\\w'],
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+            ['^.+\\.s?css$'],
+          ],
+        },
+      ],
+      'sort-imports': 'off',
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+      'unused-imports/no-unused-imports-ts': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          ignoreRestSiblings: true,
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'react/self-closing-comp': ['error', { component: true, html: true }],
+      'react/display-name': 'off',
+      'react/prop-types': 'off',
+    },
+  };
+  
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="bg-red-900">
+        <h1 className="font-bold">Portfolio 2022</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
