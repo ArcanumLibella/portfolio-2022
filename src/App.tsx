9905ae6caf0d62582bdf7ca2b88a216067b69304
Fiona <fiona.roux@hetic.net>
2022-02-13 15:10:13 +0100

    :art: Add fonts, update tailwind config and work on architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 function App() {
   return (
-    <div className="App">
-      <header className="bg-red-900">
+    <div className="App bg-purple-dark">
+      <header className="">
         <h1 className="font-bold">Portfolio 2022</h1>
       </header>
+      <main>
+        <h1 className="font-brother font-black text-md">
+          Je suis un Brother Black
+        </h1>
+        <h1 className="font-brother font-bold text-md">
+          Je suis un Brother Bold
+        </h1>
+        <h1 className="font-brother font-medium text-md">
+          Je suis un Brother Medium
+        </h1>
+        <h1 className="font-goku text-md">Je suis un Goku</h1>
+        <h1 className="font-body text-md">Je suis un Josefin Sans</h1>
+      </main>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,45 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          ultraDark: '#210044',
+          dark: '#3C0D6E',
+          DEFAULT: '#410087',
+          light: '#7729CC',
+          ultraLight: '#E4D8F0',
+        },
+        white: {
+          DEFAULT: '#FBF7FF'
+        },
+        tomato: {
+          DEFAULT: '#E94057'
+        },
+        orange: {
+          DEFAULT: '#F27121'
+        },
+      },
+      fontFamily: {
+        brother: ['Brother', 'Impact', 'sans-serif'],
+        goku: ['Goku', 'Impact', 'sans-serif'],
+        body: ['Josefin Sans', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        xxs: '.5rem', // 8px
+        xs: '.625rem', // 10px
+        tiny: '.75rem', // 12px
+        sm: '.875rem', // 14px
+        md: '.9375rem', // 15px
+        base: '1rem', // 16px
+        lg: '1.125rem', // 18px
+        xl: '1.25rem', // 20px
+        xxl: '30rem', // 120px
+      },
+    },
   },
   variants: {
     extend: {},
